1c1791f706916f8de39293b568743d1d10cf02eb
James Padgett <jampad14@gmail.com>
2018-09-24 19:23:31 -0400

    Javascript added

---
+++ main.js
@@ -0,0 +1,19 @@
+
+
+
+
+const Main = (() => {
+    document.addEventListener("DOMContentLoaded", () => {
+        console.log("JavaScript Loaded");
+    });
+    
+    // public methods[]
+    return {
+        init: () => {
+            console.log("Init");
+        }
+    }
+})();
+
+
+Main.init();
